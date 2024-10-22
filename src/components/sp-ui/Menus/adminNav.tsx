bf7b0ce4f1b443db5534b49367fe5da24261fa50
Your Name <you@example.com>
2024-10-22 00:36:43 -0600

    feat: added store orders page to manage store orders

--- src/components/sp-ui/Menus/adminNav.tsx
+++ src/components/sp-ui/Menus/adminNav.tsx
@@ -1,6 +1,5 @@
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { admin_nav_items } from './adminNavItems';
-import { MenuItems } from './menuItems';
 import { _NavSection } from './types';
 
 type Props = {
@@ -11,11 +10,12 @@ export const AdminNav = async (props: Props) => {
     <ScrollArea className="flex h-full w-full flex-col rounded-md">
       {admin_nav_items.map((item: _NavSection) => {
         return (
-          <MenuItems
-            inSheet={props.inSheet}
-            items={item.items}
-            key={`menu-item-${item.name}`}
-          />
+          <></>
+          // <MenuItems
+          //   inSheet={props.inSheet}
+          //   items={item.items}
+          //   key={`menu-item-${item.name}`}
+          // />
         );
       })}
     </ScrollArea>
