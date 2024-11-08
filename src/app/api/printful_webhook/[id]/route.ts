2ecabfdac67a91a7b66d676f012fcc01a81504d0
Your Name <you@example.com>
2024-11-08 01:20:27 -0700

    feat: added analytics to admin panel

--- src/app/api/printful_webhook/[id]/route.ts
+++ src/app/api/printful_webhook/[id]/route.ts
@@ -1,5 +1,5 @@
 import { db } from '@/lib/firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { collection, doc, DocumentData, getDoc, getDocs, query, QuerySnapshot, Timestamp, updateDoc, where, writeBatch } from 'firebase/firestore';
 import { NextRequest } from 'next/server';
 
 type Params = {
@@ -16,7 +16,7 @@ export async function POST(request: NextRequest, context: { params: Promise<Para
     const storeDoc = await getDoc(storeRef);
     if (storeDoc.exists()) {
       const syncResponse = await fetch(
-        `https://api.printful.com/store/products/${data.data.sync_product.id}`,
+        `https://api.printful.com/sync/products/${data.data.sync_product.id}`,
         {
           method: 'GET',
           headers: {
@@ -29,6 +29,72 @@ export async function POST(request: NextRequest, context: { params: Promise<Para
       console.log('SYNC RESP', syncResponse);
       const syncJson = await syncResponse.json();
       console.log('SYNC JSON', syncJson);
+      if (syncJson.code === 200) {
+        const productsRef = collection(db, 'products');
+        const q = query(
+          productsRef,
+          where('vendor_id', '==', syncJson.result.sync_product.id),
+        );
+        const productsData: QuerySnapshot<DocumentData, DocumentData> =
+          await getDocs(q);
+        const batch = writeBatch(db);
+        const imageURLs: string[] = [];
+        let price: number = 0;
+        syncJson.result.sync_variants.map((variant: any) => {
+          if (variant.availability_status === 'active') {
+            console.log(`${variant.id} Variant`, variant)
+            variant.files((file: any) => {
+              if (file.visible && file.status === 'ok') {
+                imageURLs.push(file.url);
+              }
+              console.log(`${variant.id} File`, file)
+            })
+            const newPrice = parseFloat(variant.retail_price).toFixed(2) as unknown as number;
+            if (price === 0 || price > newPrice) {
+              price = newPrice;
+            }
+            console.log(`${variant.id} PRODUCT`, variant.product)
+
+          }
+        })
+        if (productsData.empty) {
+          const newProductDoc = doc(productsRef);
+          batch.set(newProductDoc, {
+            name: syncJson.result.sync_product.name,
+            images: imageURLs,
+            vendor: 'printful',
+            vendor_id: syncJson.result.sync_product.id,
+            description: '',
+            price: price,
+            compare_at: 0,
+            currency: syncJson.result.sync_variants[0].currency,
+            inventory: 0,
+            track_inventory: false,
+            status: 'Public',
+            tags: [],
+            admin_tags: [],
+            like_count: 0,
+            product_type: '',
+            store_id: parameters.id,
+            owner_id: storeDoc.data().owner_id,
+            units_sold: 0,
+            is_featured: false,
+            created_at: Timestamp.fromDate(new Date()),
+            updated_at: Timestamp.fromDate(new Date()),
+            colors: [],
+            sku: '',
+            revenue: 0,
+            view_count: 0,
+            service_percent: 0.1,
+          });
+        } else {
+          await updateDoc(productsData.docs[0].ref, {
+            name: syncJson.result.sync_product.name
+          })
+        }
+        await batch.commit();
+      }
+
     }
   }
 
