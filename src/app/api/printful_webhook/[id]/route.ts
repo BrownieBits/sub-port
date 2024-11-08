9dbec3df61cfe350bf02f05f98ba6668b1a05670
Your Name <you@example.com>
2024-11-08 00:33:29 -0700

    feat: added analytics to admin panel

--- src/app/api/printful_webhook/[id]/route.ts
+++ src/app/api/printful_webhook/[id]/route.ts
@@ -1,3 +1,5 @@
+import { db } from '@/lib/firebase';
+import { doc, getDoc } from 'firebase/firestore';
 import { NextRequest } from 'next/server';
 
 type Params = {
@@ -9,20 +11,25 @@ export async function POST(request: NextRequest, context: { params: Promise<Para
   const parameters = await context.params;
   console.log('REQUEST', data.data);
   if (data.type === 'product_updated') {
-    const syncResponse = await fetch(
-      `https://api.printful.com/store/products/${data.data.sync_product.id}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-type': 'application/json',
-          Authorization: `Bearer 7mFSBeY8lylaUlibKaR2wMDvlWocLuLzTFhEho0I`,
-        },
-      }
-    );
-    console.log('PRODUCT UPDATE PARAMS', parameters.id);
-    console.log('SYNC RESP', syncResponse);
-    const syncJson = await syncResponse.json();
-    console.log('SYNC JSON', syncJson);
+
+    const storeRef = doc(db, 'stores', parameters.id);
+    const storeDoc = await getDoc(storeRef);
+    if (storeDoc.exists()) {
+      const syncResponse = await fetch(
+        `https://api.printful.com/store/products/${data.data.sync_product.id}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-type': 'application/json',
+            Authorization: `Bearer ${storeDoc.data().printful_access_token}`,
+          },
+        }
+      );
+      console.log('PRODUCT UPDATE PARAMS', parameters.id);
+      console.log('SYNC RESP', syncResponse);
+      const syncJson = await syncResponse.json();
+      console.log('SYNC JSON', syncJson);
+    }
   }
 
   return new Response('Success!', {
